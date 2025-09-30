fc21620e6e10ff5fa0082fb64c5d9a540d408e3f
Prajakta Yelgave <prajaktayelgave@gmail.com>
2025-09-30 16:29:24 +0530

    Added the ero section both for desktop and mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Sections/Navbar";
-
+import HeroSection from "./components/Sections/HeroSection";
 
 export default function App() {
   return (
     <ThemeProvider>
       <div>
-       
-        <Navbar/>
+        <Navbar />
+        <HeroSection />
       </div>
-     </ThemeProvider>
+    </ThemeProvider>
   );
 }
